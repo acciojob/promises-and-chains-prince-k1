54cc3c9b494065b9e48b8ef064c88b4e3a760eb3
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2024-11-29 23:44:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+let form = document.querySelector("form");
+let name = document.querySelector("#name");
+let age = document.querySelector("#age");
+form.addEventListener("submit" (e)=> {
+	e.preventDefault();
+	if(name.value == "" || age.value == "") return ;
+	let promise = new Promise((res, rej) => {
+		setTimeout(() => {
+			
+			if(age.value >= 18){
+				res(`Welcome, ${name.value}. You can vote.`)
+			}else{
+				rej(`Oh sorry ${name.value}. You aren't old enough`);
+			}
+		}, 4000)
+	})
+	promise
+		.then((res) => {
+			alert(res);
+		})
+		.catch((err) => {
+			alert(err);
+		})
+})
